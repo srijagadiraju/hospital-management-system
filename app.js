4bba5f947c9d1673c453213e8e727386f9f44f2f
hlahtoo <hlahtoo1998@gmail.com>
2023-10-09 23:24:26 -0700

    Updated frontend and JS

--- app.js
+++ app.js
@@ -5,16 +5,11 @@
 // import { errorHandler } from "./middleware/errorHandler";
 // import contactRoute from "./routes/contactRoutes";
 
-// import express from "express";
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const contactRoute = require("./routes/contactRoutes");
 const errorHandler = require("./middleware/errorHandler");
-// import morgan from "morgan";
-// import dotenv from "dotenv";
-// import { errorHandler } from "./middleware/errorHandler";
-// import contactRoute from "./routes/contactRoutes";
 
 console.log("1,2,3");
 // const morgan = require("morgan");
@@ -36,45 +31,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json()); // provides parser to help parse data stream that is received from client on the server side
 app.use("/api/contacts", contactRoute); // middleware
 app.use(errorHandler);
+app.use(morgan("dev"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-// const express = require("express");
-// const dotenv = require("dotenv").config();
-// const errorHandler = require("./middleware/errorHandler");
-// const HospitalDB = require("./HospitalDB"); // Replace with the correct path to your MongoDB utility module
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// app.use(express.json()); // Middleware for parsing JSON requests
-
-// // Define routes
-// const hospitalRoutes = require("./routes/hospitalRoutes"); // Import hospital routes
-// const patientRoutes = require("./routes/patientRoutes"); // Import patient routes
-
-// // Use routes
-// app.use("/api/hospitals", hospitalRoutes);
-// app.use("/api/patients", patientRoutes);
-
-// // Error handling middleware
-// app.use(errorHandler);
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-
-// // Database connection (if you're not using Mongoose)
-// const db = HospitalDB();
-
-// // Test database connection (optional)
-// db.read("hospitals", {})
-//   .then((result) => {
-//     console.log("Database connection established");
-//   })
-//   .catch((error) => {
-//     console.error("Error connecting to the database:", error);
-//   });
---
+++ public/js/login.js
@@ -0,0 +1,56 @@
+function login() {
+  const form = document.getElementById("login-form");
+  let signupButton = document.getElementById("signupButton");
+  let signinButton = document.getElementById("signinButton");
+  let nameField = document.getElementById("nameField");
+  let title = document.getElementById("title");
+
+  function isValidEmail(email) {
+    // This is a simple regex pattern for email validation. You can make it more complex if needed.
+    const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return pattern.test(email);
+  }
+  function handleSignIn() {
+    const emailInput = document.querySelector('#login-form input[type="text"]');
+    const idInput = document.querySelector('#login-form input[type="id"]');
+    const passwordInput = document.querySelector(
+      '#login-form input[type="Password"]'
+    );
+
+    if (!isValidEmail(emailInput.value)) {
+      alert("Please Enter a valid email address");
+      return null;
+    }
+
+    const userData = {
+      email: emailInput.value,
+      ID: idInput.value,
+      Password: passwordInput.value,
+    };
+
+    console.log(userData); // Here, you can send this data to your server or do other processing
+    return userData;
+  }
+
+  signinButton.addEventListener("click", async (e) => {
+    e.preventDefault();
+    const data = handleSignIn();
+    try {
+      let res = await fetch("./login", {
+        method: "GET",
+        body: data,
+      });
+      if (res.status === 200) {
+        sessionStorage.setItem("email", data.email);
+        window.location.href = "./request.html";
+      } else {
+        alert("You have logged in wrong email or password!");
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+    console.log(data);
+  });
+}
+
+login();
