e0c654fda3d0fb279cdd425e09f85cc120c942ec
hlahtoo <hlahtoo1998@gmail.com>
2023-10-09 19:30:31 -0700

    Completed MongoDB

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -65,91 +65,246 @@
 
 // module.exports = HospitalDB();
 
-import { MongoClient } from "mongodb";
-import fs from "fs";
-// import {
-//   getProfiles,
-//   createProfile,
-//   getProfile,
-//   updateProfile,
-//   deleteProfile,
-// } from "../controllers/contactController";
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
 
-async function getProfile(client, name) {
-  const result = await client
-    .db("Users")
-    .collection("Users-info")
-    .insertOne(newProfile); //inserts a single document into the collection
+function MongoDBUtil() {
+  const myDB = {};
+  const uri =
+    "mongodb+srv://hlahtoo1998:28H1JDxdnJqipwwL@cluster0.qfd7kqx.mongodb.net/?retryWrites=true&w=majority";
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  // const client = new MongoClient(uri);
+  const database = "Users";
+  const colRequests = "Requests";
+  const colAuthen = "Users-Login";
+
+  //-------------------------------------------------------
+  // listing databases
+  myDB.listDatabases = async () => {
+    const databasesList = await client.db().admin().listDatabases();
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      console.log("Databases: ");
+      databasesList.databases.forEach((db) => {
+        console.log(`- ${db.name}`);
+      });
+    } finally {
+      client.close();
+    }
+  };
+
+  // -----------------------------------------------------
+  // creating Request or User-Login
+  myDB.insertRequest = async (newRequest) => {
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      const result = await client
+        .db(database)
+        .collection(colRequests)
+        .insertOne({
+          name: newRequest.name,
+          id: newRequest.id,
+          department: newRequest.department,
+          item: newRequest.item,
+        }); //inserts a single document into the collection
+      console.log(
+        `New Profile created with the following id: ${result.insertedId}`
+      );
+    } finally {
+      client.close();
+    }
+  };
+
+  // -----------------------------------------------------
+  // creating User-Login
+  myDB.insertNewUser = async (newUser) => {
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      const hashedPassword = await bcrypt.hash(newUser.password, 10);
+      const result = await client.db(database).collection(colAuthen).insertOne({
+        email: newUser.email,
+        password: hashedPassword,
+      }); //inserts a single document into the collection
+      console.log(
+        `New Profile created with the following id: ${result.insertedId}`
+      );
+    } finally {
+      client.close();
+    }
+  };
+
+  // creating multiple profile
+  myDB.createMultipleProfile = async (newProfiles) => {
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      const result = await client
+        .db(database)
+        .collection(colRequests)
+        .insertMany(newProfiles);
+
+      console.log(
+        `${result.insertedCount} new profiles created with the following ids`
+      );
+      console.log(result.insertedIds);
+    } finally {
+      client.close();
+    }
+  };
+
+  // Authenticating Login
+  myDB.authenticateUsers = async (inputData) => {
+    try {
+      if (!inputData.email || !inputData.password) {
+        throw new Error("Both 'email' and 'password' fields are required.");
+      }
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      console.log("email=", inputData.email);
+      console.log("password=", inputData.password);
+      const user = await client
+        .db(database)
+        .collection(colAuthen)
+        .findOne({ email: inputData.email });
+      // check if result is not undefined
+      if (user && (await bcrypt.compare(inputData.password, user.password))) {
+        console.log("Authentication Successful!");
+        return true;
+      } else {
+        console.log("Authentication Failed!");
+        return false;
+      }
+      // if undefined, return false
+      // if defined, return true
+    } catch (error) {
+      console.error("authentication error:", error.message);
+      return false;
+    } finally {
+      client.close();
+    }
+  };
+
+  myDB.getAllRequests = async () => {
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+      // this is returning Promise {<pending >} fix this
+      // how do i make sure that it is returning the real object
+      // do i make sure when i call it or can i make sure here
+      return await client
+        .db(database)
+        .collection(colRequests)
+        .find({})
+        .toArray(); // Replace with your database name
+    } finally {
+      client.close();
+    }
+  };
+
+  myDB.deleteRequest = async (request) => {
+    try {
+      if (!request.id) {
+        throw new Error("id is required");
+      }
+      client = new MongoClient(uri);
+      await client.connect();
+      console.log("connected to MongoDB");
+
+      const result = await client
+        .db(database)
+        .collection(colRequests)
+        .deleteOne({ id: request.id });
+      // check if result is not undefined
+      if (result.deletedCount === 1) {
+        console.log("Delete Successful!");
+        return true;
+      } else {
+        console.log("Delete Failed!");
+        return false;
+      }
+      // if undefined, return false
+      // if defined, return true
+    } catch (error) {
+      console.error("Request error:", error.message);
+      return false;
+    } finally {
+      client.close();
+    }
+  };
+
+  return myDB;
 }
 
-async function createProfile(client, newProfile) {
-  const result = await client
-    .db("Users")
-    .collection("Users-info")
-    .insertOne(newProfile); //inserts a single document into the collection
-  console.log(
-    `New Profile created with the following id: ${result.insertedId}`
-  );
-}
+myDB = MongoDBUtil();
+const dave = {
+  name: "Dave Smith",
+  id: "10240",
+  department: "Cardiology",
+  item: "Ultrasound Machine",
+};
+myDB.deleteRequest(dave);
+// myDB.insertNewUser({
+//   email: "hlahtoo1998@gmail.com",
+//   password: "hlahtoo123",
+// });
 
-async function createMultipleProfile(client, newProfiles) {
-  const result = await client
-    .db("Users")
-    .collection("users-info")
-    .insertMany(newProfiles);
-
-  console.log(
-    `${result.insertedCount} new profiles created with the following ids`
-  );
-  console.log(result.insertedIds);
-}
-
-async function listDatabases(client) {
-  const databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases: ");
-  databasesList.databases.forEach((db) => {
-    console.log(`- ${db.name}`);
+/* Test case
+myDB
+  .getAllRequests()
+  .then((requests) => {
+    console.log(requests);
+  })
+  .catch((error) => {
+    console.error("Error:", error.message);
   });
-}
+*/
+// Testcase for authentications
+// myDB.authenticateUsers({
+//   email: "hlahtoo1998@gmail.com",
+//   password: "hlahtoo123",
+// });
 
-const uri =
-  "mongodb+srv://hlahtoo1998:28H1JDxdnJqipwwL@cluster0.qfd7kqx.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
+// myDB.insertRequest({
+//   name: "had we",
+//   id: "10240",
+//   department: "Cardiology",
+//   item: "Ultrasound Machine",
+// });
 
-async function main() {
-  try {
-    await client.connect();
-    await listDatabases(client);
+// User authentication
+// async function authenticateUser(client, datab, col, input) {}
 
-    await fs.readFile("../data/requests.json", "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading file:", err);
-        return;
-      }
+// async function main() {
+//   try {
+//     await client.connect();
+//     await listDatabases(client);
 
-      // Parse the JSON data into an array of objects
-      try {
-        const arrayOfObjects = JSON.parse(data);
-        createMultipleProfile(client, arrayOfObjects);
-      } catch (parseError) {
-        console.error("Error parsing JSON:", parseError);
-      }
-    });
-    // console.log(arrayOfObjects);
-    await createProfile(client, {
-      name: "Hla Htoo",
-      id: "13456",
-      department: "Pathology",
-      item: "Catheter",
-    });
-    //await listDatabases(client);
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
-
-main().catch(console.error);
+// createMultipleProfile(client, );
+//
+// console.log(arrayOfObjects);
+// await createMultipleProfile(client, database, colAuthen, [
+//   {
+//     email: "hlahtoo1998@gmail.com",
+//     password: "hlahtoo123",
+//   },
+//   {
+//     email: "srijasgadiraju@gmail.com",
+//     passsword: "srija123",
+//   },
+// ]);
+//await listDatabases(client);
+//   } catch (e) {
+//     console.log(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
